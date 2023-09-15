df1b56b3581b33d83f6d490c2c11bf2e669a6505
Andhs <106329215+Andhs-eff@users.noreply.github.com>
2023-09-15 12:42:52 +0300

    first version

--- index.js
+++ index.js
@@ -1,9 +1,77 @@
-var flag = false;
-var rec;
+console.warn = () => {};
+import { LangCode } from "./lcodes/LangCode.js";
+import {franc} from 'https://esm.sh/franc@6?bundle'
+import {langDetector} from './eld/languageDetector.js'; 
+import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.0';
+let classifier = await pipeline('automatic-speech-recognition', 'Xenova/whisper-base');
+let classifier_mms = await pipeline('audio-classification', 'Xenova/mms-lid-126');
+
+const content = document.getElementById("content");
+const talkVideo = document.getElementById('talk-video');
+const nativeReply = document.getElementById("native-reply");
+const englishReply = document.getElementById("english-reply");
+const micImage = document.getElementById("start_img")
+
+let blob;
+
+let iter = 0;
+
+let languageName = new Intl.DisplayNames(['en'], {type: 'language'});
+
+let okButton;
+
+let noButton;
+
+let recLang;
+
+let recText;
+
+let fullLang;
+
+let targetCode;
+
+let displayText;
+
+const video_codes = ["en", "ru", "es", "fr"];
+
+function setVideoElement(stream) {
+  talkVideo.src = stream;
+  talkVideo.loop = false;
+
+  // safari hotfix
+  if (talkVideo.paused) {
+    talkVideo
+      .play()
+      .then((_) => {})
+      .catch((e) => {});
+  }
+}
+
+function playIdleVideo() {
+  talkVideo.src = './speech/avatar_idle_c.mp4';
+  talkVideo.loop = true;
+}
+
+playIdleVideo();
+
+/* ************************************************************************
+* Microphone input processing 
+*/
+let flag = false;
+let rec;
 document
-  .getElementById("startRecording")
+  .getElementById('whisper')
   .addEventListener("click", toggler);
-let isRecording = document.getElementById("isRecording");
+
+function toggler() {
+if (flag) {
+    stopFunction();
+    }
+else {
+    initFunction();
+}
+}
+
 function initFunction() {
    flag = !flag;
   // Display recording
@@ -24,89 +92,31 @@ function initFunction() {
       alert("user api not supported");
     }
   }
-  document.getElementById("startRecording").textContent = "Stop"
-  isRecording.textContent = "Recording...";
-  //
+
   let audioChunks = [];
-  //let rec;
   function handlerFunction(stream) {
+    micImage.src = "./assets/mic-animate.gif";
+    micImage.classList.remove('anima');
     rec = new MediaRecorder(stream);
     rec.start();
+
+    function restartTimer() {
+        setTimeout(function() {
+            rec.stop();
+            document.getElementById("start_img").src = "./assets/mic.gif";
+        }, 8 * 1000);
+    }
+
+    // We listen only for a certain amount of time, then we form a blob
+    restartTimer();    
+
+
     rec.ondataavailable = (e) => {
       audioChunks.push(e.data);
       if (rec.state == "inactive") {
-        let blob = new Blob(audioChunks, { type: "audio/wav" });
-        console.log(blob);
-        document.getElementById("audioElement").src = URL.createObjectURL(blob);
-
-        async function sendTranscribe(blob) {
-          try {
-
-            const headers = {
-                authorization: 'ba693edc5038455084d21c63b09d632d',
-            };
-
-            const uploadResponse = await fetch("https://api.assemblyai.com/v2/upload", {
-                method: "POST",
-                headers: headers,
-                body: blob,
-                });
-
-            const uploadUrl = await uploadResponse.json();
-            console.log(uploadUrl);
-            
-            const audio_data = {
-                audio_url: uploadUrl.upload_url,
-                language_detection: true
-            };
-
-            const response = await fetch("https://api.assemblyai.com/v2/transcript", {
-                method: "POST",
-                headers: headers,
-                body: JSON.stringify(audio_data),
-                });
-
-            const transcriptId = await response.json();
-            console.log(transcriptId);
-
-            const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${transcriptId.id}`;
-   
-            while (true) {
-              const pollingResponse = await fetch(pollingEndpoint, {
-                method: "GET",
-                headers: headers
-              });
-              const transcriptionResult = await pollingResponse.json();
-              console.log(transcriptionResult);
-            
-              if (transcriptionResult.status === 'completed') {
-                console.log(transcriptionResult.text);
-                document.getElementById("asr").textContent = transcriptionResult.text;
-                break
-              } else if (transcriptionResult.status === 'error') {
-                throw new Error(`Transcription failed: ${transcriptionResult.error}`);
-              } else {
-                await new Promise((resolve) => setTimeout(resolve, 3000));
-              }
-            }
-/*              // Список транскриптов
-              const listResponse = await fetch("https://api.assemblyai.com/v2/transcript", {
-                method: "GET",
-                headers: headers
-              });
-              const listResult = await listResponse.json();
-              console.log(listResult);  */
-
-              const deleteResponse = await fetch(pollingEndpoint, { method: 'DELETE', headers: headers })
-              console.log(await deleteResponse.json())
-
-          } catch (error) {
-            console.error(error);
-          }
-      }
-            
-      sendTranscribe(blob);
-     
+        blob = new Blob(audioChunks, { type: "audio/wav" });
+        newPage();
+        langDetectAudio(blob);
       }
     };
   }
@@ -123,15 +133,311 @@ function initFunction() {
 function stopFunction() {
 	flag = !flag;
 	rec.stop();
-  document.getElementById("startRecording").textContent = "Start"
-  isRecording.textContent = "Click play button to start listening";
+    document.getElementById("start_img").src = "./assets/mic.gif";
 }
 
-function toggler() {
-	if (flag) {
-		stopFunction();
-		}
-	else {
-		initFunction();
-	}
+/* ************************************************************************
+* Text input processing 
+*/
+
+document.getElementById("intext")
+.addEventListener("input", function onEvent(event) {
+  const printed = document.getElementById("intext").value;
+  if (printed.slice(-1) == "\n") {
+    if (printed == "\n") {
+      document.getElementById("intext").value = "";
+      document.getElementsByClassName("keyboardInputInitiator")[0].classList.add('anima');
+    } else {
+    newPage();
+    langDetectText(printed);
+    }
+  } else {
+    document.getElementsByClassName("keyboardInputInitiator")[0].classList.remove('anima');
+  }
+    });
+
+/* ************************************************************************
+* Language identification 
+*/
+
+async function langDetectAudio(blob){
+
+  switch (iter) {
+      case 2:
+          [recLang, recText]  = await sendTranscribe1(blob);
+          fullLang = languageName.of(recLang);
+          break;
+      case 0:
+          [recLang, recText]  = await sendTranscribe2(blob);
+          fullLang = languageName.of(recLang);
+          break;
+      case 1:
+          recLang  = await sendTranscribe3(blob);
+          fullLang = languageName.of(recLang);
+          recText = ""; 
+          break;           
+
+  }    
+                
+    replyProcessing(fullLang, recText);
+}
+
+async function langDetectText(sourceText) {
+
+  switch (iter) {
+      case 0:
+          [recLang, recText]  = await googleTranslate("auto", "en", sourceText);
+          fullLang = languageName.of(recLang);
+          break;
+      case 1:
+          recLang  = franc(sourceText, {minLength: 6});
+          fullLang = languageName.of(recLang);
+          break;
+      case 2:
+          recLang  = langDetector.detect(sourceText)['language'];
+          fullLang = languageName.of(recLang);
+          break;           
+  }    
+                
+    replyProcessing(fullLang, sourceText);
+
+    }
+
+async function replyProcessing(recognizedLang, userText) {
+
+  if (iter == 0) {
+   
+    document.getElementById("inter").style.display = "none";
+    // document.getElementById("test-button").style.display = "none";
+
+    const ok = document.createElement('button');
+    ok.id = "ok-button";
+    ok.style.marginTop = "20px";
+    ok.style.marginRight = "200px";
+    const okImage = document.createElement('img');
+    okImage.width = 80;
+    okImage.height = 60;
+    okImage.src = "./assets/thumb-up.svg";
+
+    ok.appendChild(okImage);
+
+    const no = document.createElement('button');
+    no.id = "no-button";
+    no.style.marginTop = "20px";
+    const noImage = document.createElement('img');
+    noImage.width = 80;
+    noImage.height = 60;
+    noImage.src = "./assets/confusion.png";
+
+    no.appendChild(noImage);
+
+    content.appendChild(ok);
+    content.appendChild(no);
+
+    okButton = document.getElementById('ok-button');
+    noButton = document.getElementById('no-button');
+
+  }
+
+  targetCode = await LangCode.encode(recognizedLang);
+  if (targetCode == null) {
+    nativeReply.innerHTML = "<br/><br/>Sorry, I can't recognize the language.";
+    englishReply.innerHTML = "<br/><br/>Sorry, I can't recognize the language.";
+
+  } else {
+    if (userText != "") {
+      displayText = `You seem to be speaking <span style='color: red;'>${recognizedLang}</span><br/>Is it correct?<br/><br/>Your text:`;
+    } else {
+      displayText = `You seem to be speaking <span style='color: red;'>${recognizedLang}</span><br/>Is it correct?`;
+    }
+    const [, nativeText] = await googleTranslate("en", targetCode, displayText);
+    nativeReply.innerHTML = `${nativeText} <span style='color: seagreen;'>${userText}</span>`
+    englishReply.innerHTML = `${displayText} <span style='color: seagreen;'>${userText}</span>`
+    if (video_codes.includes(targetCode)) {
+      const stream = "./speech/" + targetCode + "_g.mp4";
+      setVideoElement(stream);
+      talkVideo.onended = playIdleVideo;
+    }
+
+  }
+
+  noButton.onclick = () => {
+
+    ++iter;
+
+    progressDisplay();
+
+    if (iter > 2) {
+      nativeReply.innerHTML = "<br/><br/>Sorry, I can't recognize the language.";
+      englishReply.innerHTML = "<br/><br/>Sorry, I can't recognize the language.";
+      goRetry();
+
+    } else if (blob != undefined) {     
+      langDetectAudio(blob);
+    } else {
+      langDetectText(userText);
+    }
+
+  };
+
+  okButton.onclick = async () => {
+    const successText = "<span style='color: blue;'>Great!</span><br/>I'm connecting you to the interpreter speaking your language.<br/>Please wait.";
+    const [, finalText] = await googleTranslate("en", targetCode, successText);
+    nativeReply.innerHTML = finalText;
+    englishReply.innerHTML = successText;
+    if (video_codes.includes(targetCode)) {
+      const stream = "./speech/" + targetCode + "_s.mp4";
+      setVideoElement(stream);
+      talkVideo.onended = playIdleVideo;
+    }
+  
+    goRetry();
+  
+  };
+
+}
+
+async function sendTranscribe1(blob) {
+  try {
+
+    const headers = {
+        authorization: 'ba693edc5038455084d21c63b09d632d',
+    };
+
+    const uploadResponse = await fetch("https://api.assemblyai.com/v2/upload", {
+        method: "POST",
+        headers: headers,
+        body: blob,
+        });
+
+    const uploadUrl = await uploadResponse.json();
+    
+    const audio_data = {
+        audio_url: uploadUrl.upload_url,
+        language_detection: true
+    };
+
+    const response = await fetch("https://api.assemblyai.com/v2/transcript", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(audio_data),
+        });
+
+    const transcriptId = await response.json();
+
+    const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${transcriptId.id}`;
+
+    while (true) {
+      const pollingResponse = await fetch(pollingEndpoint, {
+        method: "GET",
+        headers: headers
+      });
+      const transcriptionResult = await pollingResponse.json();
+    
+      if (transcriptionResult.status === 'completed') {
+        return [transcriptionResult.language_code, transcriptionResult.text];
+        // break
+      } else if (transcriptionResult.status === 'error') {
+        throw new Error(`Transcription failed: ${transcriptionResult.error}`);
+      } else {
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      }
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function sendTranscribe2(blob) {
+    try {
+    
+        let url = URL.createObjectURL(blob);
+        let result = await classifier(url);
+
+        const [codeGoogle] = await googleTranslate("auto", "en", result.text);
+
+        return [codeGoogle, result.text];
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function sendTranscribe3(blob) {
+    try {
+    
+        let url = URL.createObjectURL(blob);
+        let result = await classifier_mms(url);
+        return result[0].label;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function googleTranslate(sourceLang, targetLang, sourceText) {
+    
+    let google_url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+    
+    const googleResponse = await fetch(google_url, {
+        method: "GET",
+      });
+    const translateResult = await googleResponse.json();
+    const resArr = [];
+    for (let i = 0; i < translateResult[0].length; i++) {
+      resArr.push(translateResult[0][i][0]);
+    }
+    return [translateResult[2], resArr.join("")];
+}
+
+/* ***********************************************************
+* New page elements
+*/
+
+function progressDisplay() {
+  nativeReply.innerHTML = "";
+  const progressImage1 = document.createElement('img');
+  progressImage1.width = 80;
+  progressImage1.height = 60;
+  progressImage1.src = "./assets/progress.gif";
+  nativeReply.appendChild(progressImage1);
+  englishReply.innerHTML = "";
+  const progressImage2 = document.createElement('img');
+  progressImage2.width = 80;
+  progressImage2.height = 60;
+  progressImage2.src = "./assets/progress.gif";
+  englishReply.appendChild(progressImage2);
+}
+
+function newPage() {
+  document.getElementById("intext").style.display = "none";
+  document.getElementById("whisper").style.display = "none";
+  VKI_close();
+  document.getElementsByClassName("keyboardInputInitiator")[0].remove();
+
+  progressDisplay();
+}
+
+function goRetry() {
+  document.getElementById("ok-button").style.display = "none";
+  document.getElementById("no-button").style.display = "none";
+
+  const retry = document.createElement('button');
+  retry.id = "retry-button";
+  retry.style.marginTop = "20px";
+  const retryImage = document.createElement('img');
+  retryImage.width = 80;
+  retryImage.height = 60;
+  retryImage.src = "./assets/retry.svg";
+
+  retry.appendChild(retryImage);
+
+  content.appendChild(retry);
+
+  const retryButton = document.getElementById('retry-button');
+
+  retryButton.onclick = () => {
+    window.location.reload();
+  }
 }
---
+++ lcodes/LangCode.js
@@ -0,0 +1,24 @@
+import { CSV } from "https://js.sabae.cc/CSV.js";
+
+class LangCode {
+  static fn = "./lcodes/ISO639.csv";
+  static csv = null;
+  static async init() {
+    if (LangCode.csv) {
+      return LangCode.csv;
+    }
+    const csv = await CSV.fetch(LangCode.fn);
+    LangCode.csv = csv;
+    return csv;
+  }
+  static async encode(s) {
+    const csv = await LangCode.init();
+    const value = csv.find(line => line[1].indexOf(s) >= 0);
+    if (!value) {
+      return null;
+    }
+    return value[0];
+  }
+}
+
+export { LangCode };
